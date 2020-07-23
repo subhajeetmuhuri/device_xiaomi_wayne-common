8ec2501dbbbea83ddd6a8c49b74c3118a812fa92
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-07-23 18:23:51 +0530

    wayne-common: Move thermal-engine configs from vendor to device tree
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
    Change-Id: I4ab10ae9a40b60e7b36f79e3b41861a88b765b21

--- wayne.mk
+++ wayne.mk
@@ -53,6 +53,13 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += \
     init.device.rc
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-normal.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-video.conf
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
